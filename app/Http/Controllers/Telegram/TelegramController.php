fa2556d26d4ce1c72de7a12e663ddaf49ca0545c
hhttco <94968015+hhttco@users.noreply.github.com>
2023-11-09 12:59:08 +0800

    Update TelegramController.php

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 
 class TelegramController extends Controller
 {
+    protected $msg;
     protected $telegramService;
 
     public function __construct(Request $request)
@@ -23,6 +24,7 @@ class TelegramController extends Controller
     public function webhook(Request $request)
     {
         $this->formatMessage($request->input());
+        $this->handle();
         return;
     }
 
@@ -31,7 +33,54 @@ class TelegramController extends Controller
         if (!isset($data['message'])) return;
         if (!isset($data['message']['text'])) return;
 
-        Log::info($data);
-        Log::info($data['message']);
+        $obj = new \StdClass();
+        $text = explode(' ', $data['message']['text']);
+        $obj->command = $text[0];
+        $obj->args = array_slice($text, 1);
+        $obj->chat_id = $data['message']['chat']['id'];
+        $obj->message_id = $data['message']['message_id'];
+        $obj->message_type = 'message';
+        $obj->text = $data['message']['text'];
+        $obj->is_private = $data['message']['chat']['type'] === 'private';
+        if (isset($data['message']['reply_to_message']['text'])) {
+            $obj->message_type = 'reply_message';
+            $obj->reply_text = $data['message']['reply_to_message']['text'];
+        }
+
+        Log::info($obj);
+        $this->msg = $obj;
+    }
+
+    public function handle()
+    {
+        if (!$this->msg) return;
+        $msg = $this->msg;
+        $commandName = explode('@', $msg->command);
+
+        // To reduce request, only commands contains @ will get the bot name
+        if (count($commandName) == 2) {
+            $botName = $this->getBotName();
+            if ($commandName[1] === $botName){
+                $msg->command = $commandName[0];
+            }
+        }
+
+        try {
+        	if ($msg->message_type === 'message') {
+        		$this->telegramService->sendMessage($msg->chat_id, $this->getBotName() . "->" . $msg->text . 'message');
+        	}
+
+        	if ($msg->message_type === 'reply_message') {
+        		$this->telegramService->sendMessage($msg->chat_id, $this->getBotName() . "->" . $msg->text . ' reply_message');
+        	}
+        } catch (\Exception $e) {
+            $this->telegramService->sendMessage($msg->chat_id, $e->getMessage());
+        }
+    }
+
+    public function getBotName()
+    {
+        $response = $this->telegramService->getMe();
+        return $response->result->username;
     }
 }

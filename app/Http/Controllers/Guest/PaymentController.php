ae152ab6d8c02f4b004936361f934527445b1424
hhttco <94968015+hhttco@users.noreply.github.com>
2024-04-10 20:20:54 +0800

    Update PaymentController.php

--- app/Http/Controllers/Guest/PaymentController.php
+++ app/Http/Controllers/Guest/PaymentController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Services\AlipayService;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-	public function notify($method, Request $request)
+    public function notify(Request $request)
     {
         try {
             $paymentService = new AlipayService();
             $verify = $paymentService->notify($request->input());
+
+            Log::info("verify error" . $verify);
+
             if (!$verify) abort(500, 'verify error');
 
             Log::info("收到回调订单：" . $verify['trade_no'] . "回调：" . $verify['callback_no']);
         } catch (\Exception $e) {
+            Log::info("fail");
             abort(500, 'fail');
         }
     }
